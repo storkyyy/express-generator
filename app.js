87d62792dacaa722d4396160570a2ba267e7547a
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-11-21 21:10:29 -0500

    Week 4: Add image uploading

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
-const { RSA_NO_PADDING } = require('constants');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
@@ -60,6 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 
 // catch 404 and forward to error handler
