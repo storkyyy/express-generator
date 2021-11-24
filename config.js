122a452a8166380582db391d00c5daf1ee3e441f
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-11-23 19:14:12 -0500

    Week 4: OAuth addition with login via facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '443740747124698',
+        clientSecret: 'aaa2c902cc30939a4217093094670f38'
+    }
 }
